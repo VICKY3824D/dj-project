e3495f01af9902ce85123b7b41c76d1b04193c5e
Vidky Adhi Pradana <vickypradana1111@gmail.com>
2024-11-07 10:16:50 +0700

    Creating New Django Project

---
+++ api/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Book(models.Model):
+    pass;
+
+# Create your models here.
---
+++ playground/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# request -> response
+# request handler
+# action
+
+# def sapa(request):
+#     # pull data form db
+#     # trasform
+#     # send email
+    
+#     return HttpResponse('Hello Cuyyy ');
+
+def salam(request):
+    return render(request, 'hello.html');
